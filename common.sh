d72c7ea29af00cad7732e32dbf102209533ca66f
daws-90sb <lokam.nithisha@gmail.com>
2026-06-01 21:48:36 +0530

    changes added

--- common.sh
+++ common.sh
@@ -6,6 +6,7 @@ sudo chown -R ec2-user:ec2-user $LOGS_FOLDER
 sudo chmod -R 755 $LOGS_FOLDER
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 SCRIPT_DIR=$PWD
+MYSQL_HOST=mysql.daws-90sb.online
 
 USERID=$(id -u)
 R="\e[31m"
