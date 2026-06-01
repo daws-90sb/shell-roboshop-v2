1ff9510a8ef12c7a74ddbcb75ab4ad8521c7c713
daws-90sb <lokam.nithisha@gmail.com>
2026-06-01 17:06:15 +0530

    changes added

---
+++ 03-mysql.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+source ./common
+
+root_check
+
+
+dnf install mysql-server -y &>> $LOGS_FILE
+VALIDATE $? "Installing MYSQL server"
+
+systemctl enable mysqld   &>> $LOGS_FILE
+systemctl start mysqld    &>> $LOGS_FILE
+VALIDATE $? "Enable and Start MYSQL server"
+
+mysql_secure_installation --set-root-pass RoboShop@1
+VALIDATE $? "setting up root password"
+ 
+
+ print_total_time
